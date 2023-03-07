c819e0dfc25895ad4e7eb19da5165d1baa5e3b5f
Manipradeep <115785589+Manipradeep-mps@users.noreply.github.com>
2023-03-07 11:46:00 +0530

    Update ArrayChallenge.java

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/ArrayChallenge.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/ArrayChallenge.java
@@ -1,11 +1,20 @@
 package io.javabrains.javabasics;
-
-public class ArrayChallenge {
+        public class ArrayChallenge {
     public static void main(String[] args) {
         // Step 1: Create an array of integers
-
-
+      int[] arr={10,20,30,40,50};
         // Step 2: Use arrays and array operations to find the sum, average, and largest number in the array
+int sum=0,largest=arr[0];
+for(int i=0;i<arr.length;i++){
+    sum+=arr[i];
+    if(arr[i]>largest){
+        largest=arr[i];
+    }
+}
+double avg= (double)sum/arr.length;
+System.out.println("Sum is: "+sum);
+System.out.println("Average is: "+avg);
+System.out.println("Largest is: "+largest);
 
 
     }
