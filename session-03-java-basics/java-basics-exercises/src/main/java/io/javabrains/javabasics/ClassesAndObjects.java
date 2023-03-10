00e9f034f1824519521d47ba7a4cbc072946037c
Manipradeep <115785589+Manipradeep-mps@users.noreply.github.com>
2023-03-10 21:53:17 +0530

    Update ClassesAndObjects.java

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/ClassesAndObjects.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/ClassesAndObjects.java
@@ -17,10 +17,39 @@ Details: 2020 Toyota Camry
 ```
 
  */
+class Car {
+    private String make;
+    private String model;
+    private int year;
 
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return year + " " + make + " " + model;
+    }
+}
 public class ClassesAndObjects {
-
     public static void main(String[] args) {
-
+        Car car=new Car("Lamborghini", "Gallardo", 2013);
+        System.out.println("Make: " +car.getMake());
+        System.out.println("Model: " +car.getModel());
+        System.out.println("Year: " + car.getYear());
+        System.out.println("Details: " +car.toString());
     }
 }
