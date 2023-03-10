642e4e0742809e3998354cf63988235a161d7b02
Manipradeep <115785589+Manipradeep-mps@users.noreply.github.com>
2023-03-10 21:57:15 +0530

    Update InstanceOf.java

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
@@ -1,17 +1,27 @@
 package io.javabrains.javabasics;
 
 public class InstanceOf {
-
     public static void main(String[] args) {
-
         Object[] objects = {
                 10,
                 "Hello",
                 true,
                 20.5
         };
-
         // Challenge: Use instanceof to determine the type of each object in the array
-
+       for(Object a:objects){
+        if(a instanceof Integer){
+            System.out.println(a+" is an integer.");
+        }
+        else if(a instanceof String){
+            System.out.println(a+" is a string.");
+        }
+        else if( a instanceof Boolean){
+            System.out.println(a+" is a Boolean.");
+        }
+        else if(a instanceof Double){
+            System.out.println(a+" is a Double.");
+        }
+       }
     }
 }
